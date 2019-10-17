c540f84a47b1db7dbed94ace0f1ce7cc67a43115
ToddSing <kty5876@aol.com>
2019-10-16 23:16:50 -0400

    Put JWT token for signup and login

--- src/main/java/com/ga/controller/userController.java
+++ src/main/java/com/ga/controller/userController.java
@@ -1,8 +1,10 @@
 package com.ga.controller;
 
 import com.ga.Service.UserService;
+import com.ga.entity.JwtResponse;
 import com.ga.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,13 +18,13 @@ public class userController {
 
 
     @PostMapping("/signup")
-    public User signup(@RequestBody User user) {
-        return userservice.signup(user);
+    public ResponseEntity<?> signup(@RequestBody User user) {
+        return ResponseEntity.ok(new JwtResponse(userservice.signup(user)));
     }
 
     @PostMapping("/login")
-    public Long login(@RequestBody User user) {
-        return userservice.login(user);
+    public ResponseEntity<?> login(@RequestBody User user) {
+        return ResponseEntity.ok(new JwtResponse(userservice.login(user)));
     }
 
 
